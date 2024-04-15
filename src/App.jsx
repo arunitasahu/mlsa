a382adaedc9dbeb1c3813b36538583096300d25e
arunitasahu <arunitasahu2311@gmail.com>
2024-04-16 04:10:20 +0530

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Arunita Sahu",
+  title: "App Developer",
+  email: "aruitasahu2311@gmail.com",
+  gitHub: "github",
+  instagram: "instagram",
+  linkedIn: "linkedin",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "twitter",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/00b94bbc86f1c9830330ff57a2764f6f.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "beautiful image";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Teach edge🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is my first project",
+    url: "https://github.com/arunitasahu/teach_edge",
   },
   {
-    title: "Web Development for Beginners",
+    title: "App dev project",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "e-commerce site",
+    url: "https://github.com/arunitasahu/e_commerce",
   },
   {
-    title: "My Resume Site",
+    title: "My App",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "chatting app",
+    url: "https://github.com/arunitasahu/Planet-Messages-Group-Chat-",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "GitHub ",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Github account !",
+    url: "https://github.com/arunitasahu",
   },
 ];
 
